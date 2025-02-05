d7096677e8cb60549be07a076845cfa1604557c1
DNEK <dnek.app@gmail.com>
2025-02-05 11:56:31 +0900

    apply comfortable-controller-fader

--- nicovideo-fullscreen-keeper.user.js
+++ nicovideo-fullscreen-keeper.user.js
@@ -8,11 +8,77 @@
 // @updateURL   https://github.com/dnek/nicovideo-fullscreen-keeper/raw/main/nicovideo-fullscreen-keeper.user.js
 // @downloadURL https://github.com/dnek/nicovideo-fullscreen-keeper/raw/main/nicovideo-fullscreen-keeper.user.js
 // @match       https://www.nicovideo.jp/watch/*
-// @grant       none
+// @grant       GM_addStyle
 // @license     MIT license
 // ==/UserScript==
 
-(async () => {
+// derived from https://github.com/dnek/nicovideo-comfortable-controller-fader
+const comfortableControllerFader = () => {
+    const baseStyleEl = GM_addStyle(`
+        div[data-styling-id=":r3:"]:has(> div[data-styling-id=":r4:"]) {
+            cursor: none;
+            > div[data-styling-id=":r4:"] {
+                opacity: 0;
+                pointer-events: none;
+                &:hover {
+                    opacity: 1;
+                    pointer-events: auto;
+                }
+            }
+        }
+        `);
+
+    const mouseMovingStyleEl = GM_addStyle(`
+        div[data-styling-id=":r3:"]:has(> div[data-styling-id=":r4:"]) {
+            cursor: auto;
+            > div[data-styling-id=":r4:"] {
+                opacity: 1;
+                pointer-events: auto;
+            }
+        }
+        `);
+    mouseMovingStyleEl.disabled = true;
+
+    let timeoutID;
+
+    const initFader = () => {
+        const controllerEl = document.querySelector('div[data-styling-id=":r3:"] > div[data-styling-id=":r4:"]');
+        if (controllerEl === null) {
+            return;
+        }
+
+        controllerEl.setAttribute('nccf-controller', '');
+
+        const enableMouseMovingStyle = () => {
+            clearTimeout(timeoutID);
+            timeoutID = setTimeout(() => {
+                mouseMovingStyleEl.disabled = true;
+            }, 2000);
+            mouseMovingStyleEl.disabled = false;
+        };
+
+        enableMouseMovingStyle();
+
+        const playerEl = controllerEl.parentElement;
+
+        playerEl.addEventListener('mousemove', enableMouseMovingStyle);
+
+        playerEl.addEventListener('mouseleave', () => {
+            clearTimeout(timeoutID);
+            mouseMovingStyleEl.disabled = true;
+        });
+
+        console.log('nicovideo-comfortable-controller-fader is added.');
+    };
+
+    setInterval(() => {
+        if (document.querySelector('div[nccf-controller]') === null) {
+            initFader();
+        }
+    }, 100);
+};
+
+(() => {
     'use strict';
 
     let lastHref = location.href;
@@ -48,4 +114,7 @@
         subtree: true,
         childList: true,
     });
+
+    // When exitFullscreen is prevented, the controller does not disappear until mousemove.
+    comfortableControllerFader();
 })();
